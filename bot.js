16197759148c204f36b8a35937ad0f3d849a303d
swawesome9598 <swawesome9598@gmail.com>
2017-03-26 17:47:41 -0700

    Updates
    Added Some Commands And For The Bot To Be Playing aleebot:help

--- bot.js
+++ bot.js
@@ -8,4 +8,34 @@ client.on('message', message => {
 
  });
 
+client.on('message', message => {
+    if(message.content === 'aleebot:updates'){
+        message ('https://github.com/Alee14/AleeBot');
+    }
+
+});
+
+client.on('message', message => {
+    if(message.content === 'aleebot:ping'){
+        message.reply('Pong! :ping_pong:');
+    }
+
+});
+
+client.on('message', message => {
+    if(message.content === 'aleebot:pong'){
+        message.reply('Ping! :ping_pong:');
+    }
+
+});
+
+client.on('message', message => {
+    if(message.content === 'aleebot:help'){
+        message ('aleebot:profile\naleebot:updates\naleebot:ping\naleebot:pong');
+    }
+
+});
+
+client.on("ready", () => client.user.setGame("aleebot:help")) 
+
  client.login ('')
