3b5844a531b7a74ff28c4b3b9180e2ef61c3b211
Alee14 <alee14498@gmail.com>
2017-03-26 20:33:01 -0400

    Adding new things!!

--- bot.js
+++ bot.js
@@ -1,3 +1,7 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.login ('')
+
 client.on('message', message => {
     if(message.content === 'aleebot:profile'){
         message.reply(message.author.avatarURL);
