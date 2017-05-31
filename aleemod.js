8b74fa6b9b6936792d0e5e7d2605c64613f1e8d6
Alee14 <Alee14498@gmail.com>
2017-05-31 16:01:29 -0400

    Update aleemod.js

--- aleemod.js
+++ aleemod.js
@@ -136,19 +136,91 @@ const Discord = require('discord.js');
 	
     switch (Math.floor(Math.random() * 1000) % 27) {
         case 0:
-            presence.game.name = "Being a moderator";
+            presence.game.name = "with ban buttons";
             break; //SCRUATCHO
         case 1:
-            presence.game.name = "AleeOS";
+            presence.game.name = "AleeOS or ShiftOS";
             break;
         case 2:
             presence.game.name = "Annoy Alee";
             break;
+        case 3:
+            presence.game.name = "with an internal bug";
+            break;
+        case 4:
+            presence.game.name = "around";
+            break;
+        case 5:
+            presence.game.name = "bot games";
+            break;
+        case 6:
+            presence.game.name = "with ones and zeroes";
+            break;
+        case 7:
+            presence.game.name = "being a stepswitcher";
+            break;
+        case 8:
+            presence.game.name = "with supa weapon";
+            break;
+        case 9:
+            presence.game.name = "solving puzzles";
+            break;
+        case 10:
+            presence.game.name = "rewinding time";
+            break;
+        case 11:
+            presence.game.name = "checking archives";
+            break;
+        case 12:
+            presence.game.name = "being unbreakable";
+            break;
+        case 13:
+            presence.game.name = "sandwiches";
+            break;
+        case 14:
+            presence.game.name = "drawing pokemon";
+            break;
+        case 15:
+            presence.game.name = "obsessing";
+            break;
+        case 16:
+            presence.game.name = "the waiting game";
+            break;
+        case 17:
+            presence.game.name = "bending space";
+            break;
+        case 18:
+            presence.game.name = "with hexagons";
+            break;
+        case 19:
+            presence.game.name = "with music";
+            break;
+        case 20:
+            presence.game.name = "being a ninja";
+            break;
+        case 21:
+            presence.game.name = "if money cant buy happiness then why is it so fabulous";
+            break;
+        case 22:
+            presence.game.name = "being awesome";
+            break;
+        case 23:
+            presence.game.name = "TimeHACK";
+            break;
+        case 24:
+            presence.game.name = "TiemHARK";
+            break;
+        case 25:
+            presence.game.name = "harking tiem";
+            break;
+		case 26:
+			presence.game.name = "trying to DJ";
+			break;
     }
+client.user.setPresence(presence);
     client.user.setPresence(presence);
 }
 
-   client.login('MzE5MTI2ODI2NDczNzUwNTI4.DBCmKg.VGHbwB3-1GiDuybYi-rIam6LDoM').catch(
-function() {
+   client.login('MzE5MTI2ODI2NDczNzUwNTI4.DBCmKg.VGHbwB3-1GiDuybYi-rIam6LDoM').catch(function() {
        console.log("[ERROR] Login failed.");
    });
