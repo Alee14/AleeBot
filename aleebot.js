7162dc20647025bc3fb0b9754c46ccdd759f53c6
Unknown <Alee14498@gmail.com>
2017-06-01 22:26:22 +0000

    Moving again...

--- aleebot.js
+++ aleebot.js
@@ -13,49 +13,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./abtoken.json');
 
-
-client.on('message', message => {
-    if(message.content === 'ab:profile'){
-        message.reply(message.author.avatarURL);
-    }
-
- });
-
-client.on('message', message => {
-    if(message.content === 'ab:git'){
-        message.channel.sendMessage ('Here is the github repo: https://github.com/AleeCorp/AleeBot-AleeMod');
-    }
-
-});
-
-client.on('message', message => {
-    if(message.content === 'ab:ping'){
-        message.reply('Pong! :ping_pong:');
-    }
-
-});
-
-client.on('message', message => {
-    if(message.content === 'ab:pong'){
-        message.reply('Ping! :ping_pong:');
-    }
-
-});
-
-client.on('message', message => {
-    if(message.content === 'ab:help'){
-        message.channel.sendMessage ('```Commands for AleeBot!\n\nab:profile\nab:git\nab:ping\nab:pong\nab:owner```');
-    }
-
-});
-
-client.on('message', message => {
-    if(message.content === 'ab:owner'){
-		message.channel.sendMessage ('The person who made this is Alee14#9928!');
-    }
-
-});
-
   client.on('ready', () => {
        console.log("[SUCCESS] AleeBot is now ready!");
     client.setInterval(setGame, 300000);
@@ -153,6 +110,50 @@ client.on('message', message => {
     }
 client.user.setPresence(presence);
 }
+
+
+client.on('message', message => {
+    if(message.content === 'ab:profile'){
+        message.reply(message.author.avatarURL);
+    }
+
+ });
+
+client.on('message', message => {
+    if(message.content === 'ab:git'){
+        message.channel.sendMessage ('Here is the github repo: https://github.com/AleeCorp/AleeBot-AleeMod');
+    }
+
+});
+
+client.on('message', message => {
+    if(message.content === 'ab:ping'){
+        message.reply('Pong! :ping_pong:');
+    }
+
+});
+
+client.on('message', message => {
+    if(message.content === 'ab:pong'){
+        message.reply('Ping! :ping_pong:');
+    }
+
+});
+
+client.on('message', message => {
+    if(message.content === 'ab:help'){
+        message.channel.sendMessage ('```Commands for AleeBot!\n\nab:profile\nab:git\nab:ping\nab:pong\nab:owner```');
+    }
+
+});
+
+client.on('message', message => {
+    if(message.content === 'ab:owner'){
+		message.channel.sendMessage ('The person who made this is Alee14#9928!');
+    }
+
+});
+
 
  client.login (config.token).catch(function() {
        console.log("[ERROR] Login failed.");
