30d5893725ba73ba34044888492daf1e548ec13d
Unknown <Alee14498@gmail.com>
2017-06-01 20:55:42 +0000

    So yeah...

--- runbot.bat
+++ runbot.bat
@@ -3,6 +3,6 @@
 echo [INFO] Getting the latest info...
 git pull
 echo [SUCCESS] Finished getting the latest info
-echo [INFO] Starting AleeBot
-node bot.js
+echo [INFO] Starting AleeBot & AleeMod
+node aleebot.js && node aleemod.js
 goto 1
