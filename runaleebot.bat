9e10da1cc2f0673712b512cb6b5bd51977a40e15
Unknown <Alee14498@gmail.com>
2017-06-01 21:25:07 +0000

    MOAR UPDATES!!!!

--- runaleebot.bat
+++ runaleebot.bat
@@ -3,6 +3,6 @@
 echo [INFO] Getting the latest info...
 git pull
 echo [SUCCESS] Finished getting the latest info
-echo [INFO] Starting AleeBot
+echo [INFO] Starting AleeBot...
 node aleebot.js
 goto 1
