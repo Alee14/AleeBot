c3eaae117114f437e421ec27ac904896f01f684c
Unknown <Alee14498@gmail.com>
2017-08-10 22:48:33 -0400

    Doing stuff

--- abblessed.js
+++ abblessed.js
@@ -1,63 +1,46 @@
 var blessed = require('blessed');
+var abversion = "1.0.9 Beta";
 
 // Create a screen object.
 var screen = blessed.screen({
   smartCSR: true
 });
 
-screen.title = 'my window title';
+screen.title = 'AleeBot'+ abversion +'Console';
 
-// Create a box perfectly centered horizontally and vertically.
-var box = blessed.box({
-  top: 'center',
-  left: 'center',
-  width: '50%',
-  height: '50%',
-  content: 'Hello {bold}world{/bold}!',
-  tags: true,
-  border: {
-    type: 'line'
-  },
-  style: {
-    fg: 'white',
-    bg: 'magenta',
-    border: {
-      fg: '#f0f0f0'
+
+
+var titleBox = blessed.text({
+
+    top: "0",
+
+    left: "0",
+
+    width: "100%",
+
+    height: "1",
+
+    content: "AleeBot " + abversion + " Console",
+
+    tags: true,
+
+    style: {
+
+        fg: 'black',
+
+        bg: 'white'
+
     },
-    hover: {
-      bg: 'green'
+
+    padding: {
+
+        left: 1
+
     }
-  }
+
 });
 
-// Append our box to the screen.
-screen.append(box);
-
-// Add a png icon to the box
-var icon = blessed.image({
-  parent: box,
-  top: 0,
-  left: 0,
-  type: 'overlay',
-  width: 'shrink',
-  height: 'shrink',
-  file: __dirname + '/my-program-icon.png',
-  search: false
-});
-
-// If our box is clicked, change the content.
-box.on('click', function(data) {
-  box.setContent('{center}Some different {red-fg}content{/red-fg}.{/center}');
-  screen.render();
-});
-
-// If box is focused, handle `enter`/`return` and give us some more content.
-box.key('enter', function(ch, key) {
-  box.setContent('{right}Even different {black-fg}content{/black-fg}.{/right}\n');
-  box.setLine(1, 'bar');
-  box.insertLine(1, 'foo');
-  screen.render();
-});
+screen.append(titleBox);
 
 // Quit on Escape, q, or Control-C.
 screen.key(['escape', 'q', 'C-c'], function(ch, key) {
