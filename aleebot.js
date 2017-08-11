4123cf8e02317466ccd08ca66b876cacbe32221a
Alee14 <Alee14498@gmail.com>
2017-08-11 16:08:16 -0400

    Adding a new command

--- aleebot.js
+++ aleebot.js
@@ -92,6 +92,7 @@ client.on("message", function(message){
 			"**"+prefix+"ping** Type this command and you'll see ;).\n" +
 			'**'+prefix+'owner** Shows who made this bot.\n' +
 			'**'+prefix+'suggest** You suggest things (working progress).\n' +
+      '**'+prefix+'uptime** It tells you how long AleeBot has been on for.\n' +
 			'**'+prefix+'userinfo** Tells you your info about you.\n' +
 			'**'+prefix+'botinvite** Lets you bring AleeBot to your server.\n' +
 			'**'+prefix+'serverinvite** Invites you to the AleeArmy Server.\n' +
@@ -120,6 +121,46 @@ client.on("message", function(message){
 
     if(command === 'suggest'){
         message.reply('Sorry this feature is still being worked on :(');
+    }
+
+    if(command === 'uptime'){
+      var timeString; // What we'll eventually put into the message
+
+      var uptime = parseInt(client.uptime); // Get uptime in ms
+
+      uptime = Math.floor(uptime / 1000); // Convert from ms to s
+
+      var uptimeMinutes = Math.floor(uptime / 60); // Get the uptime in minutes
+
+      var minutes = uptime % 60;
+
+      var hours = 0;
+
+      while (uptimeMinutes >= 60) {
+
+      hours++;
+
+      uptimeMinutes = uptimeMinutes - 60;
+
+}
+
+
+
+if (uptimeMinutes < 10) {
+
+    timeString = hours + ":0" + uptimeMinutes // We need to add an additional 0 to the minutes
+
+} else {
+
+    timeString = hours + ":" + uptimeMinutes // We don't need to add an extra 0.
+
+}
+
+
+
+message.reply(":clock1: AleeBot has been up for " + timeString + " hours.");
+
+commandProcessed = true;
     }
 
     if(command === 'userinfo'){
