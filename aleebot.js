73ed3ef4d363f29f5833ccd3696e3841d66570cd
Alee14 <Alee14498@gmail.com>
2017-08-12 14:02:47 -0400

    Eval Command

--- aleebot.js
+++ aleebot.js
@@ -266,11 +266,30 @@ commandProcessed = true;
 	    return;
       message.channel.sendMessage(args.join(" "));
       message.delete();
-
     
     }
+  if(command === 'eval'){
+  if(message.author.id !== config.ownerID) return;
+    try {
+      const code = args.join(" ");
+      let evaled = eval(code);
 
+      if (typeof evaled !== "string")
+        evaled = require("util").inspect(evaled);
+
+      message.channel.send(clean(evaled), {code:"xl"});
+    } catch (err) {
+      message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+       }
+     }
  });
+
+const clean = text => {
+  if (typeof(text) === "string")
+    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+  else
+      return text;
+}
 
  process.on('unhandledRejection', function(err, p) {
    console.log("[ERROR | UNCAUGHT PROMISE] " + err.stack);
