277ede320961f833d02fffa666c8c98d2364f049
Alee14 <Alee14498@gmail.com>
2017-08-14 01:58:33 -0400

    Remove something

--- aleebot.js
+++ aleebot.js
@@ -329,9 +329,6 @@ commandProcessed = true;
      }
     if(command === 'ship'){
       message.channel.send(":ship: "+ message.author.username + " x " + message.guild.members.random().displayName);
-    } else if {
-          message.reply("Erm... This isn't a command buddy if you need help type `ab:help`.")
-    }
 
 
  });
