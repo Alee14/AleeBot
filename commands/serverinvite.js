eba5cb75ac74c8eb9a9ed7c1137faf570b4cec07
Unknown <Alee14498@gmail.com>
2017-08-24 20:14:25 -0400

    New server invite link

--- commands/serverinvite.js
+++ commands/serverinvite.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 exports.run = (client, message, args, config) => {
   message.reply(':arrow_left: Continue in DMs.');
-  message.author.sendMessage("You want a invite to the AleeArmy server? Here's the link: https://discord.gg/JqgXrAJ");
+  message.author.sendMessage("You want a invite to the AleeArmy server? Here's the link: https://discord.gg/97vUx56");
 }
 
 exports.conf = {
