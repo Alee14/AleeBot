290986c1914424cf0dad054ce39bc665516ee792
Alee14 <Alee14498@gmail.com>
2017-10-03 18:14:28 -0400

    Fixes

--- aleebot.js
+++ aleebot.js
@@ -36,24 +36,7 @@ const log = message => {
 
 };
 
-function setGame() {
-    var games = [
-        "with version " + abversion,
-        "attacking AstralMod",
-        "with stuff",
-        "with Alee",
-        "games"
-    ]
 
-    client.user.setPresence({
-        status: 'online',
-        afk: false,
-        game: {
-            type: 0,
-            name: games[Math.floor(Math.random() * games.length)]
-        }
-    })
-}
 
 client.commands = new Discord.Collection();
 
@@ -74,6 +57,24 @@ fs.readdir('./commands/', (err, files) => {
 
 client.on('ready', () => {
     log(`[>] AleeBot is now ready! Running version ${abversion} in ${client.guilds.size} guilds!`);
+    function setGame() {
+    var games = [
+        "with version " + abversion,
+        "attacking AstralMod",
+        "with stuff",
+        "with Alee",
+        "games"
+    ]
+
+    client.user.setPresence({
+        status: 'online',
+        afk: false,
+        game: {
+            type: 0,
+            name: games[Math.floor(Math.random() * games.length)]
+        }
+    })
+}
     client.user.setStatus('online')
 });
 
