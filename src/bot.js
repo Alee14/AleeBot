7b00dd543ba5270241605a1dcf5ff7f06d94e291
Alee14 <Alee14498@gmail.com>
2017-11-10 19:05:09 -0500

    Removed abwelcome and added the git command

--- src/bot.js
+++ src/bot.js
@@ -59,7 +59,7 @@ client.on("message", function(message) {
         var embed = new Discord.RichEmbed()
             .setAuthor('AleeBot ' + abVersion + ` Help and on ${client.guilds.size} servers`, "https://cdn.discordapp.com/avatars/282547024547545109/6c147a444ae328c38145ef1f74169e38.png?size=2048")
             .setDescription("Every command you input into AleeBot is `" + config.prefix + "`")
-            .addField("- General Commands", "ping\nuptime", true)
+            .addField("- General Commands", "ping\nuptime\ngit", true)
             .setFooter("AleeCorp Copyright 2017")
             .setColor("#1fd619")
         message.channel.sendEmbed(embed);
@@ -81,6 +81,10 @@ client.on("message", function(message) {
 			}
 			var uptimeSeconds = minutes % 60;
         message.channel.send(":clock3: AleeBot has been up for " + hours + " hours, " + uptimeMinutes + " minutes, and " + uptimeSeconds + " seconds.")
+    }
+
+    if (command === 'git') {
+        message.author.send("I can see you want to contribute to this project.\nHere's the link: https://github.com/AleeCorp/AleeBot")
     }
 
 });
