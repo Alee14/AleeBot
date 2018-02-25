c35bceee4ea05d1cac87609bd6e76d998b12ad17
Alee <alee14498@gmail.com>
2018-02-24 20:26:44 -0500

    Made some changes on changelog

--- src/commands/changelog.js
+++ src/commands/changelog.js
@@ -1,9 +1,9 @@
 module.exports.run = async (client, message) => {
   const Discord = require('discord.js');
   const embed = new Discord.RichEmbed()
-    .setAuthor('AleeBot ' + '2.0.0 beta' + 'Changelog', 'https://cdn.discordapp.com/avatars/282547024547545109/6c147a444ae328c38145ef1f74169e38.png?size=2048')
+    .setAuthor('AleeBot ' + '2.0.0 Beta ' + 'Changelog', 'https://cdn.discordapp.com/avatars/282547024547545109/6c147a444ae328c38145ef1f74169e38.png?size=2048')
     .setDescription('What\'s new in AleeBot?')
-    .addField('+ Rewritten command handler', true)
+    .addField('+ Rewritten command handler (Written by jtsshieh big thanks to him!)', true)
     .addField('+ New uptime command (Thanks to Rain)', true)
     .addField('? Some commands are the same from 1.x', true)
     .setFooter('AleeCorp Copyright 2017')
