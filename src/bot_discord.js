b41c18e497f17208d3eed9bd14a378647348255a
Alee <alee14498@gmail.com>
2018-03-02 22:40:09 -0500

    Started working on 2.1.0

--- src/bot_discord.js
+++ src/bot_discord.js
@@ -7,8 +7,8 @@
  **********************************************/
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const abVersion = '2.0.0';
-const prefix = 'ab:';
+const abVersion = '2.1.0 Beta';
+const prefix = 'abb:';
 const fs = require('fs');
 const config = require('./absettings.json');
 console.log('Welcome to AleeBot NodeJS Terminal!');
