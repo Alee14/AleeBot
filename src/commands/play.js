b374cbdce8fe314013f24c1ae90f5f063fa61df0
Unknown <jtsshieh@outlook.com>
2018-03-24 19:57:00 -0400

    thanks guy from d.js server for noticing my spelling mistake

--- src/commands/play.js
+++ src/commands/play.js
@@ -50,7 +50,7 @@ module.exports.run = async (client, message, args) => {
       .addField('Link', video.url)
       .addField('Duration', time)
       .setThumbnail(video.thumbnails.high.url)
-      .setFootor('Beat Music Player');
+      .setFooter('Beat Music Player');
     await message.channel.send({embed});
     if (!client.voiceConnections.get(message.member.guild.id))
       message.member.voiceChannel.join().then(function(connection) {
