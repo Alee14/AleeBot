5764b61a0f8a715cfa06d98ef82f577ad9fc8e29
Alee <alee14498@gmail.com>
2018-03-25 14:39:58 -0400

    Added reasons to ban and kick, removed days

--- commands/ban.js
+++ commands/ban.js
@@ -23,16 +23,13 @@
  * SOFTWARE.
  ********************************/
 module.exports.run = async (client, message, args) => {
-    let mreason = args.join(" ").slice(22);
-
+    const mreason = args.join(" ").slice(22);
     if (!message.member.permissions.has('BAN_MEMBERS')) return message.reply("It looks like that you don't have the permissions to ban people.")
+    if (!message.guild.member(client.user).hasPermission('BAN_MEMBERS')) return message.reply('Uhh... I don\'t have permission to ban members.');
     const member = message.mentions.members.first();
     if (!member) return message.reply("Uhh... Please mention a member first.");
-    member.ban({
-        days: args[1] || null,
-        reason: `Banned by ${message.author.tag} ${mreason}`
-    });
-    message.reply("User Banned! Reason: " + mreason);
+    member.ban(`Banned by ${message.author.tag} Reason: ` + mreason);
+    message.reply(member.user.tag + " has been banned! Reason: " + mreason);
 };
 
 exports.conf = {
--- commands/kick.js
+++ commands/kick.js
@@ -22,12 +22,14 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  ********************************/
-module.exports.run = async (client, message) => {
+module.exports.run = async (client, message, args) => {
+    const mreason = args.join(" ").slice(22);
     if (!message.member.permissions.has('KICK_MEMBERS')) return message.reply("It looks like that you don't have the permissions to ban people.")
+    if (!message.guild.member(client.user).hasPermission('KICK_MEMBERS')) return message.reply('Uhh... I don\'t have permission to kick members.');
     const member = message.mentions.members.first();
     if (!member) return message.reply("Uhh... Please mention a member first.");
-    member.kick(`Kicked by: ${message.author.tag}`);
-    message.reply("User Kicked!");
+    member.kick(`Kicked by: ${message.author.tag} Reason: ` + mreason);
+    message.reply(member.user.tag + ` has been kicked! Reason: ` + mreason);
 };
 
 exports.conf = {
