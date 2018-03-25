0f1f12f10a869c5f297f036aab76871015852e07
Alee <alee14498@gmail.com>
2018-03-25 12:53:04 -0400

    Added suggestions

--- commands/changelog.js
+++ commands/changelog.js
@@ -25,10 +25,9 @@
 module.exports.run = async (client, message) => {
   const Discord = require('discord.js');
   const embed = new Discord.RichEmbed()
-    .setAuthor('AleeBot ' + '2.3.0 ' + 'Changelog', 'https://cdn.discordapp.com/avatars/282547024547545109/6c147a444ae328c38145ef1f74169e38.png?size=2048')
-    .setDescription('What\'s new in AleeBot 2.3?')
-    .addField('[>] Purge Command!','Purge command only for moderators!', true)
-    .addField('[>] Say Command!','Say command is only for the person who created the bot!', true)
+    .setAuthor('AleeBot ' + '2.4.0 Beta ' + 'Changelog', 'https://cdn.discordapp.com/avatars/282547024547545109/6c147a444ae328c38145ef1f74169e38.png?size=2048')
+    .setDescription('What\'s new in AleeBot 2.4?')
+    .addField('[>] Suggestion Command!','Suggest command is only for exclusive servers!', true)
     .setFooter('AleeCorp Copyright 2017')
     .setColor('#1fd619');
   message.channel.sendEmbed(embed);
---
+++ commands/suggest.js
@@ -0,0 +1,50 @@
+/********************************
+ * 
+ * Suggest: Command for AleeBot
+ * 
+ * Copyright (c) 2018 AleeCorp
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ ********************************/
+module.exports.run = async (client, message, args) => {
+    if (message.guild.id != '243022206437687296') return message.reply ('This is a ACN exclusive command.');
+    const { RichEmbed } = require('discord.js');
+    client.channels.find('id', '427495678390960148').send(
+        new RichEmbed()
+          .setColor ('#1fd619')
+          .setTitle('Suggestion')
+          .setDescription(`This is a suggestion from `+ message.author.username +` please react to it using the following emojis.`)
+          .addField('Suggestion Contents', args.join(' '))
+      ).then(message => {
+        message.react('\u2705');
+        message.react('\u274E');
+      });
+  };
+  
+  exports.conf = {
+    aliases: [],
+    guildOnly: true,
+  };
+  exports.help = {
+    name: 'suggest',
+    description: 'Suggest things in AleeBot.',
+    usage: 'suggest [suggestion]',
+    category: '- Exclusive Commands',
+  };
+  
