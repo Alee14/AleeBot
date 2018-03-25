d07e68e033d9046b4b90190436fa84865d981b35
Alee <alee14498@gmail.com>
2018-03-24 23:02:07 -0400

    uhhhhhhhhh oops

--- src/commands/say.js
+++ src/commands/say.js
@@ -2,7 +2,7 @@ module.exports.run = async (client, message, args) => {
     if (!['242775871059001344',].includes(message.author.id)) return message.reply('Nope! You need the person who created this bot to use this command.');
     let absay = args.join(" ");
     message.delete().catch();
-    message.reply(absay);
+    message.channel.send(absay);
   };
   
   exports.conf = {
