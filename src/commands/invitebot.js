b33609a43dcad72b2d7b6dc64190037198cd7e53
Alee <alee14498@gmail.com>
2018-03-24 22:09:18 -0400

    Continue in DMs

--- src/commands/invitebot.js
+++ src/commands/invitebot.js
@@ -1,4 +1,5 @@
 module.exports.run = async (client, message) => {
+    message.reply("Continue in DMs.")
     message.author.send('Want AleeBot in your server? Here\'s the link: https://discordapp.com/oauth2/authorize?client_id=282547024547545109&permissions=68185158&scope=bot');
   };
   
