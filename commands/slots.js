f7c1a068fe13fec7e3fc64527744110b1e8364e3
Alee <alee14498@gmail.com>
2018-03-31 13:29:42 -0400

    Changed the license from MIT to GPL

--- commands/slots.js
+++ commands/slots.js
@@ -1,5 +1,5 @@
-/** **************************************
- *
+/****************************************
+ * 
  *   Slots: Command for AleeBot
  *   Copyright (C) 2018 AleeCorp & jtsshieh + PokeWorld
  *
@@ -15,7 +15,7 @@
  *
  *   You should have received a copy of the GNU General Public License
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * 
  * *************************************/
 
 module.exports.run = async (client, message) => {
