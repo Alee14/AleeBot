f64827b626984ef723884984c515353aac54ec6a
Alee <Alee14498@gmail.com>
2018-05-29 12:32:03 -0400

    Update run_win.bat

--- run_win.bat
+++ run_win.bat
@@ -3,7 +3,7 @@ title AleeBot Console
 :main
 echo Welcome to the AleeBot Console.
 echo If you want to self-host this bot, please continue by executing the following steps:
-echo 1. Create a absettings.json file and include the token of your bot user so that the bot does not error out when connecting to Discord Servers.
+echo 1. Create a tokens.json file and include the token of your bot user so that the bot does not error out when connecting to Discord Servers.
 echo 2. You will need to edit this file that you ran, and remove the 'git pull" line. This causes errors as it will try to pull from the AleeBot git.
 echo 3. Finally, you must credit the developers (AleeCorp Developers will work fine).
 echo --------------------------------------------------------------------------------
