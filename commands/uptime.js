f63a68e1d0406756aac0ff72179416ab2870391a
Alee <Alee14498@gmail.com>
2018-05-29 11:58:16 -0400

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -1,7 +1,7 @@
 /****************************************
  * 
  *   Uptime: Command for AleeBot
- *   Copyright (C) 2018 AleeCorp
+ *   Copyright (C) 2018 AleeCorp & OfficialRain
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
