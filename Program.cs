e1270a2ad34726ac31a41812d41d390d82258358
Andrew Lee <alee14498@protonmail.com>
2020-07-24 16:02:45 -0400

    Added Discord.NET stuff

--- Program.cs
+++ Program.cs
@@ -1,12 +1,50 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
 
 namespace AleeBot
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        public async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+
+            // Remember to keep token private or to read it from an 
+            // external source! In this case, we are reading the token 
+            // from an environment variable. If you do not know how to set-up
+            // environment variables, you may find more information on the 
+            // Internet or by using other methods such as reading from 
+            // a configuration.
+            await _client.LoginAsync(TokenType.Bot, 
+                "token");
+            await _client.StartAsync();
+            
+            _client.MessageReceived += MessageReceived;
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+        
+        private async Task MessageReceived(SocketMessage message)
+        {
+            if (message.Content == "!ping")
+            {
+                await message.Channel.SendMessageAsync("Pong!");
+            }
+        }
+        
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
     }
 }
