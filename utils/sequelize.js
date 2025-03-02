9d2e7b26ad067fd13fb6862eb48a6b86a5bb8afd
Andrew Lee <andrew@alee14.me>
2025-03-02 01:04:07 -0500

    Starting AleeBot 4.0!

--- utils/sequelize.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize('database', 'user', 'password', {
-    host: 'localhost',
-    dialect: 'sqlite',
-    logging: false,
-    storage: 'database.sqlite',
-});
-
-module.exports = sequelize;
